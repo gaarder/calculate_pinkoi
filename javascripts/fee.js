125cabbdd61b398c31d303a4711dd8afc8c16dbe
Sammy Lin <sammylintw@gmail.com>
2013-06-23 16:38:00 +0800

    init

---
+++ javascripts/fee.js
@@ -0,0 +1,31 @@
+function FeeCtrl($scope) {
+  $scope.Total = 0;
+  $scope.change = function() {
+    var fee,Totalfee = 0;
+    Totalfee = parseInt($scope.Credit) + parseInt($scope.Mart);
+    $scope.Total = Totalfee;
+    
+    $scope.Pinkoi = ($scope.Credit * 0.1) + ($scope.Mart * 0.1);
+    $scope.FeeTax = ((($scope.Credit * 0.15)+10) + (($scope.Mart * 0.1)+15)) * 1.05;
+
+
+    fee = $scope.Total - $scope.FeeTax; 
+    if (fee >= 1000)
+    {
+      $scope.BankFee = 45;
+      $scope.Remittance =  fee + 45;
+    }else if(fee <= 0)
+    {
+      $scope.BankFee = 0;
+      $scope.Remittance = fee + '(你賣太少了啦，反而要倒貼...)';
+    }else
+    {
+      $scope.BankFee = 0;
+      $scope.Remittance = fee + '(不足1000元累積至下個月份)';
+    }
+
+
+  }
+
+}
+
